e4ca8c834e9157236175948a808f9e936a9c3e9d
Aiswarya125 <aiswarya.j@gadgeon.com>
2022-10-15 13:16:28 +0530

    My first spa project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const appRoute:Routes=
+[
+  {path:"", component:SigninComponent},
+  {path:"signIn", component:SigninComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"",redirectTo:'/signIn', pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  
+register!:FormGroup
+
+  constructor(private formbuilder:FormBuilder, private http:HttpClient, private route: Router) { }
+
+  ngOnInit(): void {
+    this.register = this.formbuilder.group(
+      {
+        name:[''],
+        email:[''],
+        password:[''],
+        confirmpass:['']
+      }
+    )
+  }
+  signUp(){
+    this.http.post<any>( 'http://localhost:3000/users',this.register.value).subscribe(res=>
+    {
+      alert("Success");
+    })
+  }
+
+}
